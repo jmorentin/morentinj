d3e542510c9de83b722a5272ca3cb27a87bc5d41
Justin Morentin <jmorentin717@gmail.com>
2019-07-28 22:44:34 +0000

    finished 16

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,7 @@
 class Quote < ApplicationRecord
+def invalid
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
   validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
+end
+
